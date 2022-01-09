6a9b68662fc7b80be7f375a425f77da8983f8fa6
hdsenevi <shanaka36@gmail.com>
2022-01-09 12:29:31 +1100

    work in progress

--- ios/AppleWallet.m
+++ ios/AppleWallet.m
@@ -46,14 +46,14 @@ RCT_EXPORT_METHOD(canAddCard:(NSString *)cardId
 RCT_EXPORT_METHOD(isCardInWallet:(NSString *)card
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject) {
-
+    
     PKPassLibrary *library = [[PKPassLibrary alloc] init];
     if (![library canAddPaymentPassWithPrimaryAccountIdentifier:card]) {
         // If the card cannot be added to the wallet, there is no way we can find it there.
         resolve(@NO);
         return;
     }
-
+    
     NSArray* passes = [library passesOfType:PKPassTypePayment];
     for (int i=0; i < [passes count]; i++) {
         PKPaymentPass* pass = [passes objectAtIndex:i];
@@ -71,37 +71,37 @@ RCT_EXPORT_METHOD(callAddPaymentPassRequestHandler:(NSDictionary *)args
                   reject:(RCTPromiseRejectBlock)reject) {
     PKAddPaymentPassRequest *paymentPassRequest = [[PKAddPaymentPassRequest alloc] init];
     RCTLogInfo(@"In callAddPaymentPassRequestHandler:");
-//    RCTLogInfo(@"activationData %@", args[@"activationData"]);
-//    RCTLogInfo(@"encryptedPassData %@", args[@"encryptedPassData"]);
-//    RCTLogInfo(@"ephemeralPublicKey %@", args[@"ephemeralPublicKey"]);
+    RCTLogInfo(@"activationData %@", args[@"activationData"]);
+    RCTLogInfo(@"encryptedPassData %@", args[@"encryptedPassData"]);
+    RCTLogInfo(@"ephemeralPublicKey %@", args[@"ephemeralPublicKey"]);
     
     if (paymentPassRequest != nil) {
-//        paymentPassRequest.activationData = [[NSData alloc] initWithBase64EncodedString:args[@"activationData"] options:0];
-//        paymentPassRequest.encryptedPassData = [[NSData alloc] initWithBase64EncodedString:args[@"encryptedPassData"] options:0];
-//        paymentPassRequest.ephemeralPublicKey = [[NSData alloc] initWithBase64EncodedString:args[@"ephemeralPublicKey"] options:0];
+        //        paymentPassRequest.activationData = [[NSData alloc] initWithBase64EncodedString:args[@"activationData"] options:0];
+        //        paymentPassRequest.encryptedPassData = [[NSData alloc] initWithBase64EncodedString:args[@"encryptedPassData"] options:0];
+        //        paymentPassRequest.ephemeralPublicKey = [[NSData alloc] initWithBase64EncodedString:args[@"ephemeralPublicKey"] options:0];
         
         paymentPassRequest.activationData = args[@"activationData"];
         paymentPassRequest.encryptedPassData = args[@"encryptedPassData"];
         paymentPassRequest.ephemeralPublicKey = args[@"ephemeralPublicKey"];
     }
     RCTLogInfo(@"in module paymentPassRequest: %@", paymentPassRequest);
-
+    
     if (self.addPaymentPassRequestCompletionHandler != nil) {
         RCTLogInfo(@"Invoking completion handler");
         self.addPaymentPassRequestCompletionHandler(paymentPassRequest);
     } else {
         RCTLogInfo(@"Completion handler was not set");
     }
-
+    
 }
 
 - (NSDictionary *)constantsToExport {
     PKAddPassButton *addPassButton = [[PKAddPassButton alloc] initWithAddPassButtonStyle:PKAddPassButtonStyleBlack];
     [addPassButton layoutIfNeeded];
-
+    
     PKPaymentButton *paymentButton = [[PKPaymentButton alloc] initWithPaymentButtonType:PKPaymentButtonTypeBuy paymentButtonStyle:PKPaymentButtonStyleBlack];
     [paymentButton layoutIfNeeded];
-
+    
     return @{
         @"AddPassButtonWidth": @(CGRectGetWidth(addPassButton.frame)),
         @"AddPassButtonHeight": @(CGRectGetHeight(addPassButton.frame)),
@@ -141,32 +141,32 @@ RCT_EXPORT_METHOD(callAddPaymentPassRequestHandler:(NSDictionary *)args
 RCT_EXPORT_METHOD(presentAddPaymentPassViewController: (NSDictionary *)args
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-
+    
     PKAddPaymentPassRequestConfiguration *configuration = [[PKAddPaymentPassRequestConfiguration alloc] initWithEncryptionScheme:PKEncryptionSchemeECC_V2];
-
+    
     self.cardholderName = args[@"cardholderName"];
     self.localizedDescription = args[@"localizedDescription"];
     self.paymentNetwork = PKPaymentNetworkMasterCard;
     self.primaryAccountSuffix = args[@"primaryAccountSuffix"];
-    self.primaryAccountIdentifier = args[@"primaryAccountIdentifier"];
-
+//    self.primaryAccountIdentifier = args[@"primaryAccountIdentifier"];
+    
     configuration.cardholderName = self.cardholderName;
-    configuration.localizedDescription = self.localizedDescription;
-    configuration.paymentNetwork = self.paymentNetwork;
+//    configuration.localizedDescription = self.localizedDescription;
+//    configuration.paymentNetwork = self.paymentNetwork;
     configuration.primaryAccountSuffix = self.primaryAccountSuffix;
-    configuration.primaryAccountIdentifier = self.primaryAccountIdentifier;
-
+//    configuration.primaryAccountIdentifier = self.primaryAccountIdentifier;
+    
     PKAddPaymentPassViewController *passView = [[PKAddPaymentPassViewController alloc] initWithRequestConfiguration:configuration
                                                                                                            delegate:self];
     if (passView != nil) {
         dispatch_async(dispatch_get_main_queue(), ^{
             UIApplication *sharedApplication = RCTSharedApplication();
             UIWindow *window = sharedApplication.keyWindow;
-
+            
             RCTLogInfo(@"before %@", self.nonce);
             if (window) {
                 UIViewController *rootViewController = window.rootViewController;
-
+                
                 if (rootViewController) {
                     [rootViewController presentViewController:passView animated:YES completion:^{
                         RCTLogInfo(@"after %@", self.nonce);
@@ -175,7 +175,7 @@ RCT_EXPORT_METHOD(presentAddPaymentPassViewController: (NSDictionary *)args
                         resolve(nil);
                         return;
                     }];
-
+                    
                 }
             }
         });
@@ -194,28 +194,28 @@ RCT_EXPORT_METHOD(presentAddPaymentPassViewController: (NSDictionary *)args
                       nonceSignature:(nonnull NSData *)nonceSignature
                    completionHandler:(nonnull void (^)(PKAddPaymentPassRequest * _Nonnull))handler {
     RCTLogInfo(@"addPaymentPassViewController delegate to generate cert chain, nonce and nonce signature");
-
+    
     self.addPaymentPassRequestCompletionHandler = handler;
-
+    
     // the leaf certificate will be the first element of that array and the sub-CA certificate will follow.
     self.leafCertificate = [[certificates objectAtIndex:0] base64EncodedStringWithOptions:0];
     self.subCACertificate = [[certificates objectAtIndex:1] base64EncodedStringWithOptions:0];
     self.nonce = [nonce base64EncodedStringWithOptions:0];
     self.nonceSignature = [nonceSignature base64EncodedStringWithOptions:0];
-
+    
     NSMutableDictionary *args = [[NSMutableDictionary alloc] initWithCapacity:4];
     [args setObject:self.leafCertificate forKey:@"leafCertificate"];
     [args setObject:self.subCACertificate forKey:@"subCACertificate"];
     [args setObject:self.nonce forKey:@"nonce"];
     [args setObject:self.nonceSignature forKey:@"nonceSignature"];
-
+    
     RCTLogInfo(@"Event send to JS with certs, nonce and nonceSignature");
     [self sendEventWithName:@"generatedCertChainAndNonce" body:@{@"args" : args}];
 }
 
 - (void)addPaymentPassViewController:(nonnull PKAddPaymentPassViewController *)controller didFinishAddingPaymentPass:(nullable PKPaymentPass *)pass error:(nullable NSError *)error {
     NSLog(@"pass: %@ | error: %@", pass, error);
-
+    
     if (pass != nil) {
         [self sendEventWithName:@"addingPassSucceeded" body:nil];
     } else {
@@ -224,12 +224,12 @@ RCT_EXPORT_METHOD(presentAddPaymentPassViewController: (NSDictionary *)args
                 @"code" : @([error code]),
                 @"message" : [error localizedDescription],
             }];
-
+            
         } else {
             [self sendEventWithName:@"addingPassFailed" body:nil];
         }
     }
-
+    
     [controller dismissViewControllerAnimated:YES
                                    completion:^() {
         // should controller be released here
